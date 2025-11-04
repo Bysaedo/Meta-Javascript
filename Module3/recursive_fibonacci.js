3d74b33f1a006aa056894de2613dae03af59eae4
Bysaedo <by.saedo@gmail.com>
2025-11-03 21:55:23 -0600

    Functional programming paradigm

--- Module3/recursive_fibonacci.js
+++ Module3/recursive_fibonacci.js
@@ -8,3 +8,27 @@ function fib(n) {
 }
 console.log(fib(5));
 console.log(fib(10));
+
+//Functional programming paradigm
+function addTwoNums(a, b) {
+  console.log(a + b);
+}
+
+function randomNum() {
+  return Math.floor(Math.random() * 10 + 1);
+}
+function specificNum() {
+  return 42;
+}
+
+var useRandom = true;
+
+var getNumber;
+
+if (useRandom) {
+  getNumber = randomNum;
+} else {
+  getNumber = specificNum;
+}
+
+addTwoNums(getNumber(), getNumber());
