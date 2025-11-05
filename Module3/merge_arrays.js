7b72fa05664ba52e4f8df88455096cd852fa3829
Bysaedo <by.saedo@gmail.com>
2025-11-05 00:31:43 -0600

    Completed spread and rest activity

---
+++ Module3/merge_arrays.js
@@ -0,0 +1,19 @@
+const favoriteBaseballTeams = [
+  "Yankees",
+  "Mets",
+  "Dodgers",
+  "Giants",
+  "Red Sox",
+  "Cubs",
+];
+const favoriteFootballTeams = [
+  "Patriots",
+  "Giants",
+  "Jets",
+  "Eagles",
+  "Steelers",
+  "Cowboys",
+];
+const favoriteTeams = [...favoriteBaseballTeams, ...favoriteFootballTeams];
+
+console.log("Favorite teams:", favoriteTeams);
