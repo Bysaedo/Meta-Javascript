34859cad9cf4fe7ace057da88aa26f8faf00bb1e
Bysaedo <by.saedo@gmail.com>
2025-11-05 18:25:24 -0600

    Completed jest testing activity

---
+++ Module4/jest-testing/timesTwo.js
@@ -0,0 +1,7 @@
+// Task 1: Code the timesTwo function declaration
+function timesTwo(number) {
+  var doubleN = number * 2;
+  return doubleN;
+}
+// Task 2: Export the timesTwo function as a module
+module.exports = timesTwo;
